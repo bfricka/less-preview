504a1d6182f80025a3a9fbfbab0cdabbe9117893
brian-frichette <brian.frichette@gmail.com>
2013-12-02 08:21:32 -0800

    Version updates

--- express/less-options.js
+++ express/less-options.js
@@ -1,8 +1,11 @@
 module.exports = {
     versions: [
-      { label: "1.4.1 (current)", number: "1.4.1", type: "current" }
+    { label: "1.5.1 (current)", number: "1.5.1", type: "current" }
+    , { label: "1.5.0", number: "1.5.0", type: "old" }
+    , { label: "1.4.2", number: "1.4.2", type: "old" }
+    , { label: "1.4.1", number: "1.4.1", type: "old" }
     , { label: "1.4.0", number: "1.4.0", type: "old" }
-    , { label: "1.3.3", number: "1.3.3", type: "old"}
+    , { label: "1.3.3", number: "1.3.3", type: "old" }
     , { label: "1.3.2", number: "1.3.2", type: "old" }
     , { label: "1.3.1", number: "1.3.1", type: "old" }
     , { label: "1.3.0", number: "1.3.0", type: "old" }
