305eff32949db4414f574fa1fdc0abde4a6e64c4
Brian Frichette <brian.frichette@gmail.com>
2014-03-11 01:22:19 -0700

    Sleeep

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,11 @@
+var _ = require('lodash');
+var path = require('path');
 
+function directory() {
+  return path.join.apply(path, [].slice.call(arguments).map(function(dir) {
+    return path.normalize(dir);
+  }));
+}
 
 module.exports = function(grunt) {
   var locals = {
@@ -7,10 +14,29 @@ module.exports = function(grunt) {
       js      : './public/javascripts',
       tmp     : './tmp',
       test    : './test',
+      build   : './build',
+      tasks   : './build/tasks',
       routes  : './routes',
       express : './express'
     }
   };
+
+  _.forIn(locals.paths, function(p, key) {
+    locals.paths[key] = path.normalize(p);
+  });
+
+  // Add grunt-config-merge
+  require(directory(locals.paths.build, 'grunt-config-merge'))(grunt, locals);
+
+  var tasks = [
+    'get-javascripts',
+    'meta',
+    'concat',
+    'uglify',
+    'watch'
+  ].forEach(function(task) {
+    require(directory(locals.paths.tasks))(grunt, locals);
+  });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
