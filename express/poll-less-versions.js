e13bb8b316dc25ccea77ea6cd28854b4e96b05bb
Brian Frichette <brian.frichette@gmail.com>
2014-11-08 01:27:33 -0800

    Polling

--- express/poll-less-versions.js
+++ express/poll-less-versions.js
@@ -1,9 +1,10 @@
 var Stopwatch = require('stopwatch-emitter').Stopwatch;
 var stopwatch = new Stopwatch('10m');
+var lessVersions = require('./less-versions');
 
 stopwatch.on('start', pollVersions);
 stopwatch.on('restart', pollVersions);
 
 function pollVersions() {
-
+  lessVersions().then();
 }
